b006b0bc64e2ac94ea406d00de495e91b438bc68
Janne Hietamäki <janne.hietamaki@gmail.com>
2009-12-26 23:58:40 +0200

    cleaning up

--- mungbean-clojure/project.clj
+++ mungbean-clojure/project.clj
@@ -1,1 +1,6 @@
-(defproject mungbean-clojure "1.0.0-SNAPSHOT" :description "FIXME: write" :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"] [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
+(defproject mungbean-clojure "1.0.0-SNAPSHOT" 
+:description "MongoDB driver for Clojure"
+:dependencies [
+    [org.clojure/clojure "1.1.0-master-SNAPSHOT"] 
+    [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+])
