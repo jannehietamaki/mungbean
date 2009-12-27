d70ca5a31dfbe95b88e0a49b08609f4f2ec1eecd
Janne Hietamäki <janne.hietamaki@gmail.com>
2009-12-27 23:25:15 +0200

    missing from last commit

--- mungbean-clojure/src/mungbean.clj
+++ mungbean-clojure/src/mungbean.clj
@@ -1,1 +1,33 @@
 (ns mungbean)
+
+(defn get-db [name]
+(.openDatabase (new mungbean.clojure.ClojureMungbean "localhost" 27017) name)
+)
+
+(defn get-collection [db name]
+(.openCollection db name)
+)
+
+(defn insert [collection, doc]
+(.insert collection doc)
+)
+
+(defn delete [collection, query]
+(.delete collection query)
+)
+
+(defn update [collection, query, doc, upsert]
+(.update collection query doc upsert)
+)
+
+(defn query [collection, rules, first, items]
+(seq (.query collection rules first items))
+)
+
+(defn find-one [collection, id]
+(.find collection id)
+)
+
+(defn command [db, cmd]
+(.command db cmd)
+)
--- mungbean-java/project/build/src/MungBean.scala
+++ mungbean-java/project/build/src/MungBean.scala
@@ -2,8 +2,11 @@ import sbt._
 
 class MongoDriver(info: ProjectInfo) extends DefaultProject(info) {
       val objenesis = "org.objenesis" % "objenesis" % "1.2"
+      val clojure = "org.clojure" % "clojure" % "1.0.0"
+
       val junit = "junit" % "junit" % "4.7" %  "test->default"       
       val jdave = "org.jdave" % "jdave-junit4" % "1.1" % "test->default"
+
 
       val lpRepo = "Laughing Panda repository" at "http://www.laughingpanda.org/maven2"
 }
