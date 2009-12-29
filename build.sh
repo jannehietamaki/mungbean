bb1ddfee9084dbca270732b3ea6d6e3ce34ea2af
Janne Hietamäki <janne.hietamaki@gmail.com>
2009-12-29 16:50:26 +0200

    improved build script

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+set -e
 cd mungbean-java
 ../bin/sbt test package
+cd ../mungbean-clojure
+rm -f lib/mungbean*.jar
+cp ../mungbean-java/target/scala_2.7.7/*.jar lib/
+../bin/lein test
+../bin/lein jar
 cd ..
+
