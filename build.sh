19d82ed0419dd71c8fe1b3d9d459a08bc4f5e87c
Janne Hietamäki <janne.hietamaki@gmail.com>
2010-09-04 11:41:36 +0300

    updated dependencies

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd mungbean-java
 ../bin/sbt test package
 cd ../mungbean-clojure
 rm -f lib/mungbean*.jar
-cp ../mungbean-java/target/scala_2.7.7/*.jar lib/
+cp ../mungbean-java/target/scala_2.8.0/*.jar lib/
 ../bin/lein test
 ../bin/lein jar
 cd ..
--- mungbean-java/project/build/src/MungBean.scala
+++ mungbean-java/project/build/src/MungBean.scala
@@ -7,7 +7,8 @@ class MongoDriver(info: ProjectInfo) extends DefaultProject(info) {
       val objenesis = "org.objenesis" % "objenesis" % "1.2"
       val clojure = "org.clojure" % "clojure" % "1.1.0"
 
-      val junitInterface = "com.novocode" % "junit-interface" % "0.3" % "test->default"
+
+      val junitInterface = "com.novocode" % "junit-interface" % "0.4" % "test->default"    
       val jdave = "com.github.mpeltonen" % "jdave-junit4" % "1.2-beta1" % "test->default"
 
       val lpRepo = "JDave snapshots" at "http://www.laughingpanda.org/maven2/"
