676e1621c6128da4359843cbd3bc3949a6a5b2b4
Janne Hietamäki <janne.hietamaki@gmail.com>
2010-09-04 12:23:25 +0300

    initial maven build

--- mungbean-clojure/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject mungbean-clojure "1.0.0-SNAPSHOT" 
-:description "MongoDB driver for Clojure"
-:dependencies [
-    [org.clojure/clojure "1.1.0"] 
-    [org.clojure/clojure-contrib "1.1.0"]
-])
